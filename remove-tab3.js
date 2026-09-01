ae76d8cb362f0f54c2399ef4c3a69c99df9eec33
ahmed gebaly <hmdsmalibaly@gmail.com>
2026-09-01 23:25:59 +0300

    chore: remove leftover temporary script

--- remove-tab3.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('d:/kbs-system/packer.html', 'utf8');
-
-html = html.replace(/\/\/ 7\. Orders List Logic \(Tab 3\)[\s\S]*?renderPackerOrders\(e\.target\.value\);\s*\}\);/, '');
-
-fs.writeFileSync('d:/kbs-system/packer.html', html, 'utf8');
-console.log('Removed tab 3 logic');
